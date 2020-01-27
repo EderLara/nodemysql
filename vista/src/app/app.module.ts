3946537b72d31ae4ee47a1dff7bc3463d9b19eae
Eder Lara Trujillo <ederlatru@gmail.com>
2020-01-26 21:31:42 -0500

    Adicionamos los componentes de la vista

--- vista/src/app/app.module.ts
+++ vista/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ReportesComponent } from './componentes/reportes/reportes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    RegistroComponent,
+    ReportesComponent
   ],
   imports: [
     BrowserModule,
